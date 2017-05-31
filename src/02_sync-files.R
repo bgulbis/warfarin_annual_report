9f9cd6f956a4434af17a16183061749e5704f1ed
bgulbis <brian.gulbis@gmail.com>
2017-05-31 14:13:48 -0500

    update sync files script

--- src/02_sync-files.R
+++ src/02_sync-files.R
@@ -2,24 +2,25 @@ library(aws.s3)
 library(tidyverse)
 library(edwr)
 library(httr)
+library(dirr)
 
 # prevent peer checking due to MH firewall
 set_config(config(ssl_verifypeer = 0L))
 
 bucket <- "warfarin-annual-report"
+read_dir <- "data/raw"
+save_dir <- paste0(read_dir, "/")
 
+# list of files with data from MBO
 mbo_files <- c("demographics", "diagnosis", "labs", "meds-inpt",
                "order-actions", "patients", "procedures", "warfarin-info")
 
+# list of files with data from EDW
 edw_files <- c("encounters", "identifiers")
 
-files <- map(mbo_files, ~read_data("data/raw", .x, FALSE))
-names(files) <- mbo_files
-# list2env(files, .GlobalEnv)
+# read data files into data frames then save each data frame to S3
+files <- map(mbo_files, ~read_data(read_dir, .x, FALSE))
+save_rds_s3(files, mbo_files, bucket, save_dir)
 
-# walk2(files, names(files), ~write_rds(.x, path = paste0("data/raw/", .y, ".Rds"), compress = "gz"))
-walk2(files, mbo_files, ~s3saveRDS(.x,
-                                   object = paste0("data/raw/", .y, ".Rds"),
-                                   bucket = bucket,
-                                   headers = list("x-amz-server-side-encryption" = "AES256"))
-)
+files <- map(edw_files, ~read_data(read_dir, .x))
+save_rds_s3(files, edw_files, bucket, save_dir)
