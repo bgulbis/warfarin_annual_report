84eb62b75bb4ec8b50e3148ca5586f91100230de
bgulbis <brian.gulbis@gmail.com>
2017-08-14 15:17:39 -0500

    add function to make inr ranges

--- src/helper_functions.R
+++ src/helper_functions.R
@@ -35,3 +35,50 @@ make_indications <- function(d) {
         dplyr::rowwise() %>%
         dplyr::mutate_(.dots = setNames(dots, "other"))
 }
+
+make_inr_ranges <- function(raw.data) {
+    # make sure we are only working with INR range data, remove and empty values
+    dots <- list(~warfarin.event == "inr range", ~warfarin.result != "")
+    tidy <- dplyr::filter_(raw.data, .dots = dots)
+
+    fix_ranges <- function(x, y) {
+        tidy <<- purrrlyr::dmap_at(.d = tidy, .at = "warfarin.result",
+                                   .f = stringr::str_replace_all,
+                                   pattern = stringr::regex(x, ignore_case = TRUE),
+                                   replacement = y)
+    }
+
+    find <- c("(INR|Goal)|-\\.|\\(.*\\)|=",
+              "\\.\\.",
+              "--|to|/",
+              "[0-9\\.]+( )[0-9\\.]+",
+              "[1-9\\.]+([0])[1-9\\.]+",
+              "(>|above|greater[ than]?)[ ]?([0-9\\.]+)",
+              "(<|below|less[ than]?)[ ]?([0-9\\.]+)",
+              "^1.[5-9]$", "^2$", "^2.[1-4]$", "^2.5$", "^2.[6-9]$", "^3$", "^3.5$")
+
+    replace <- c("", ".", "-", "-", "-", "\\2-3.5", "1.5-\\2",
+                 "1.5-2", "1.5-2.5", "2-2.5", "2-3", "2.5-3", "2.5-3.5", "3-4")
+
+    # perform string replacements to clean up inr ranges
+    purrr::walk2(.x = find, .y = replace, .f = fix_ranges)
+
+    # separate the inr range into two columns, goal low and high
+    tidy <- tidyr::extract_(data = tidy, col = "warfarin.result",
+                            into = c("goal.low", "goal.high"),
+                            regex = "([0-9\\.]+ ?)-( ?[0-9\\.]+)",
+                            remove = FALSE, convert = TRUE)
+
+    # correct any goals like "200", which should be "2.0", or "25" = "2.5"
+    fix_div <- function(y, n) {
+        purrr::as_vector(purrr::map_if(y, ~ !is.na(.x) && .x >= n, ~ .x / n))
+    }
+
+    tidy <- purrrlyr::dmap_at(.d = tidy, .at = c("goal.low", "goal.high"),
+                           .f = fix_div, n = 100)
+    tidy <- purrrlyr::dmap_at(.d = tidy, .at = c("goal.low", "goal.high"),
+                           .f = fix_div, n = 10)
+
+    dots <- list(quote(-warfarin.result), quote(-warfarin.event))
+    tidy <- dplyr::select_(tidy, .dots = dots)
+}
