eba95716adff1fd00f9fb11d9c487fe156f05af6
Brian Gulbis <brian.gulbis@gmail.com>
2018-08-06 14:28:22 -0500

    find patients, store in separate folder

--- src/01_make-patient-list.R
+++ src/01_make-patient-list.R
@@ -1,45 +1,25 @@
 # make list of patient encounters
 
-# run MBO query
-#   * Patients - by Medication (Generic) - Administration Date
-#       - Date and Time - Administration: set to desired time frame
-
 library(tidyverse)
 library(lubridate)
 library(edwr)
 
-dir_raw <- "data/raw"
+dir_raw <- "data/raw/fy2018"
+
+if (!dir.exists(dir_raw)) dir.create(dir_raw)
 
 # compress data files
 dirr::gzip_files(dir_raw)
 
-# get list of patients already pulled
-completed_pts <- "data/final/patients_completed.csv"
-pulled <- tibble("millennium.id" = "")
-
-if (file.exists(completed_pts)) {
-    pulled <- read_csv(completed_pts, col_types = "c?cdc")
-}
+# run MBO query
+#   * Patients - by Medication (Generic) - Administration Date
+#       - Date and Time - Administration: set to desired time frame
 
 # generate list of patients to retrieve data
 raw_patients <- read_data(dir_raw, "patients", FALSE) %>%
-    as.patients() %>%
-    arrange(millennium.id)
+    as.patients()
 
-to_pull <- anti_join(raw_patients, pulled, by = "millennium.id")
-
-all_mbo <- concat_encounters(raw_patients$millennium.id)
-id_mbo <- concat_encounters(to_pull$millennium.id)
-
-save_pts <- raw_patients %>%
-    filter(!is.na(discharge.datetime))
-
-if (!file.exists(completed_pts)) {
-    x <- FALSE
-} else {
-    x <- TRUE
-}
-write_csv(save_pts, completed_pts, append = x)
+id_mbo <- concat_encounters(raw_patients$millennium.id)
 
 # run MBO queries:
 #   * Orders - Actions
@@ -61,10 +41,7 @@ write_csv(save_pts, completed_pts, append = x)
 persons <- read_data(dir_raw, "identifiers") %>%
     as.id()
 
-persons_pull <- anti_join(persons, pulled, by = "millennium.id")
-
 id_edw <- concat_encounters(persons_pull$person.id)
-all_edw <- concat_encounters(persons$person.id)
 
 # run EDW queries:
 #   * Encounters - by Person ID
